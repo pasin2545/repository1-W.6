31d6b06477401e6ff624c4a328e903f7a6b2a650
PASIN KEAWNIL <63010656@KMITL.AC.TH>
2020-09-19 22:04:20 +0700

    add matrix

--- repository1 W.6/repos1.cpp
+++ repository1 W.6/repos1.cpp
@@ -1,24 +1,32 @@
 #include<stdio.h>
-#include<math.h>
 
 int main() {
-	int num, p[20] , re[20];
-	scanf_s("%d", &num);
-	if (num >= 1 && num <= 20) {
-		for (int i = 0; i < num; i++) {
-			scanf_s("%d", &p[i]);
-			if (p[i] >= 1 && p[i] <= 214) {
-				re[i] = pow(2, p[i]);
-			}
-		}
-		for (int j = 0; j < num; j++) {
-			if (p[j] >= 1 && p[j] <= 214) {
-				printf("%d\n", re[j]);
-			}
-			else {
-				printf("\n");
-			}
-		}
-	}
-	return 0;
+    int matrix[100][100];
+    int re = 0;
+    int m, n;
+    scanf_s("%u %u", &m, &n);
+    if (m >= 1 && n >= 1) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                scanf_s("%d", &matrix[i][j]);
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j && matrix[i][j] != 1) {
+                    re = re + 1;
+                }
+                else if (i != j && matrix[i][j] != 0) {
+                    re = re + 1;
+                }
+            }
+        }
+        if (re == 0) {
+            printf("Identity Matrix");
+        }
+        else {
+            printf("Not Identity Matrix");
+        }
+    }
+    return 0;
 }
